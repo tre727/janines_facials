3c647f5f20c578e0d2b94a2ff64ddc0f05042c36
trevon <tre.cunningham0727@gmail.com>
2023-12-16 22:26:41 -0500

    first commit

---
+++ src/Home/Home.js
@@ -0,0 +1,11 @@
+import './Home.scss'
+import logo from '../assets/images/Janine’s Facials.svg'
+
+function Home(){
+    return(
+        <>
+            <img className='home__logo' src={logo}></img>
+        </>
+    );
+}
+export default Home;
